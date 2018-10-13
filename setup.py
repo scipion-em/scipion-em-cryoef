dd030aaa6ef4ea39f43ca5efeec84cc7a8663703
Gregory Sharov <sharov.grigory@gmail.com>
2018-10-13 19:20:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -65,12 +65,12 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0b1',  # Required
+    version='1.0.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='Cryoef  ready to use in scipion.',  # Required
+    description='Plugin to use Cryoef program within the Scipion framework',  # Required
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
@@ -90,99 +90,113 @@ setup(
 
     # This should be your name or the name of the organization which owns the
     # project.
-    author='I2PC',  # Optional
+    author='Grigory Sharov',  # Optional
 
     # This should be a valid email address corresponding to the author listed
     # above.
-    author_email='scipion@cnb.csic.es',  # Optional
+    author_email='sharov.grigory@gmail.com',  # Optional
 
-    # Classifiers help users find your project by categorizing it.
-    #
-    # For a list of valid classifiers, see
-    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    #classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-     #   'Development Status :: 3 - Alpha',
+        # Classifiers help users find your project by categorizing it.
+     #
+     # For a list of valid classifiers, see
+     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+     classifiers=[  # Optional
+         # How mature is this project? Common values are
+         #   3 - Alpha
+         #   4 - Beta
+         #   5 - Production/Stable
+         'Development Status :: 4 - Beta',
 
-        # Indicate who your project is intended for
-     #   'Intended Audience :: Users',
+         # Indicate who your project is intended for
+         #   'Intended Audience :: Users',
 
-        # Pick your license as you wish
-     #   'License :: OSI Approved :: MIT License',
+         # Pick your license as you wish
+         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-     #   'Programming Language :: Python :: 2.7'
-    #],
+         # Specify the Python versions you support here. In particular, ensure
+         # that you indicate whether you support Python 2, Python 3 or both.
+         'Programming Language :: Python :: 2.7'
+     ],
 
-    # This field adds keywords for your project which will appear on the
-    # project page. What does your project relate to?
-    #
-    # Note that this is a string of words separated by whitespace, not a list.
-    keywords='scipion cryoem imageprocessing scipion-1.2',  # Optional
+     # This field adds keywords for your project which will appear on the
+     # project page. What does your project relate to?
+     #
+     # Note that this is a string of words separated by whitespace, not a list.
+     keywords='scipion electron-microscopy cryo-em structural-biology image-processing',  # Optional
 
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    # Alternatively, if you just want to distribute a single Python file, use
-    # the `py_modules` argument instead as follows, which will expect a file
-    # called `my_module.py` to exist:
-    #
-    #   py_modules=["my_module"],
-    #
-    #packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
-    packages=find_packages(),
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    #install_requires=['peppercorn'],  # Optional
+     # You can just specify package directories manually here if your project is
+     # simple. Or you can use find_packages().
+     #
+     # Alternatively, if you just want to distribute a single Python file, use
+     # the `py_modules` argument instead as follows, which will expect a file
+     # called `my_module.py` to exist:
+     #
+     #   py_modules=["my_module"],
+     #
+     #packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+     packages=find_packages(),
+     # This field lists other packages that your project depends on to run.
+     # Any package you put here will be installed by pip when your project is
+     # installed, so they must be valid existing projects.
+     #
+     # For an analysis of "install_requires" vs pip's requirements files see:
+     # https://packaging.python.org/en/latest/requirements.html
+     #install_requires=['peppercorn'],  # Optional
 
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). Users will be able to install these using the "extras"
-    # syntax, for example:
-    #
-    #   $ pip install sampleproject[dev]
-    #
-    # Similar to `install_requires` above, these must be valid existing
-    # projects.
-    #extras_require={  # Optional
-    #    'dev': ['check-manifest'],
-    #    'test': ['coverage'],
-    #},
+     # List additional groups of dependencies here (e.g. development
+     # dependencies). Users will be able to install these using the "extras"
+     # syntax, for example:
+     #
+     #   $ pip install sampleproject[dev]
+     #
+     # Similar to `install_requires` above, these must be valid existing
+     # projects.
+     #extras_require={  # Optional
+     #    'dev': ['check-manifest'],
+     #    'test': ['coverage'],
+     #},
 
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.
-    #
-    # If using Python 2.6 or earlier, then these have to be included in
-    # MANIFEST.in as well.
-    # include_package_data=True,
-    package_data={  # Optional
-       'cryoef': [''],
-    },
+     # If there are data files included in your packages that need to be
+     # installed, specify them here.
+     #
+     # If using Python 2.6 or earlier, then these have to be included in
+     # MANIFEST.in as well.
+     # include_package_data=True,
+     #package_data={  # Optional
+     #   'cryoef': ['cryoef_logo.jpg'],
+     #},
 
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    #
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],  # Optional
+     # Although 'package_data' is the preferred approach, in some case you may
+     # need to place data files outside of your packages. See:
+     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+     #
+     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+     #data_files=[('my_data', ['data/data_file'])],  # Optional
 
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
-    #entry_points={  # Optional
-    #    'console_scripts': [
-    #        'sample=sample:main',
-    #    ],
-    #},
-)
+     # To provide executable scripts, use entry points in preference to the
+     # "scripts" keyword. Entry points provide cross-platform support and allow
+     # `pip` to create the appropriate form of executable for the target
+     # platform.
+     #
+     # For example, the following would provide a command called `sample` which
+     # executes the function `main` from this package when invoked:
+     #entry_points={  # Optional
+     #    'console_scripts': [
+     #        'sample=sample:main',
+     #    ],
+     #},
+
+     # List additional URLs that are relevant to your project as a dict.
+     #
+     # This field corresponds to the "Project-URL" metadata fields:
+     # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
+     #
+     # Examples listed include a pattern for specifying where the package tracks
+     # issues, where the source is hosted, where to say thanks to the package
+     # maintainers, and where to support the project financially. The key is
+     # what's used to render the link text on PyPI.
+     project_urls={  # Optional
+         'Bug Reports': 'https://github.com/scipion-em/scipion-em-cryoef/issues',
+         'Source': 'https://github.com/scipion-em/scipion-em-cryoef/',
+     },
+ )
