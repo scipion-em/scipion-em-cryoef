47a94c4d3f99a6ca4862fb4aea10a59506bde29e
Grigory Sharov <sharov.grigory@gmail.com>
2022-07-02 16:11:14 +0100

    add possible outputs

--- cryoef/__init__.py
+++ cryoef/__init__.py
@@ -32,7 +32,7 @@ from pyworkflow.utils import Environ
 from cryoef.constants import CRYOEF_HOME, V1_1_0
 
 
-__version__ = '3.0.13'
+__version__ = '3.0.14'
 _logo = "cryoEF_logo.png"
 _references = ['Naydenova2017']
 
